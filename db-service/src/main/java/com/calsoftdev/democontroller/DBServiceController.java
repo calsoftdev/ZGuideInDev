e457a2ec2f27cda0320b9e26877f0c97b0ae049a
Nilesh Repale <nilesh.repale@calsofthq.com>
2019-10-16 14:05:21 +0530

    DB service snapshot-01

---
+++ db-service/src/main/java/com/calsoftdev/democontroller/DBServiceController.java
@@ -0,0 +1,45 @@
+package com.calsoftdev.democontroller;
+
+import java.io.Console;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.calsoftdev.model.Student;
+import com.calsoftdev.repository.DBserviceRepository;
+
+@RestController
+@RequestMapping("/db-service")
+public class DBServiceController {
+	
+	private DBserviceRepository dbserviceRepository;
+
+	public DBServiceController(DBserviceRepository dbserviceRepository) {
+		super();
+		this.dbserviceRepository = dbserviceRepository;
+	}
+
+	@GetMapping("/getstudent/{studentName}")
+	public List<Student> getName(@PathVariable("studentName") final String studentName){
+		return dbserviceRepository.findByStudentName(studentName);
+				//.stream().map(Student::getStudentName).collect(Collectors.toList());
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Student> addStudentInfo(@RequestBody Student student){
+		try {
+			dbserviceRepository.save(student);
+			return new ResponseEntity<Student>(HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.GONE);
+		}
+	}
+}
---
+++ db-service/src/main/java/com/calsoftdev/repository/DBserviceRepository.java
@@ -0,0 +1,11 @@
+package com.calsoftdev.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.calsoftdev.model.Student;
+
+public interface DBserviceRepository extends JpaRepository<Student, Integer>{
+	public List<Student> findByStudentName(String studentName);
+}
